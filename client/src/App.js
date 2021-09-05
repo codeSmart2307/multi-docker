3a833505a39ad840bd4006c1454a827edcbf37c7
codeSmart2307 <raneesh2307@gmail.com>
2021-09-05 12:57:18 +0530

    add deployment config

--- client/src/App.js
+++ client/src/App.js
@@ -12,7 +12,7 @@ class App extends Component {
         <div className="App">
           <header className="App-header">
             <img src={logo} className="App-logo" alt="logo" />
-            <h2>Fibonacci Calculator</h2>
+            <h2>Fibonacci Calculator v1</h2>
             <Link to="/">Home</Link>
             <Link to="/otherpage">Other Page</Link>
           </header>
